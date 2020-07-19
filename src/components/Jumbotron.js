1597bff45e115f87ed853d2ad633b2b405ac0190
Justin Marchi <marchij86@gmail.com>
2020-07-19 18:48:21 -0400

    App working and design stylized

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -2,9 +2,9 @@ import React from "react";
 
 function Jumbotron() {
     return (
-        <div className="jumbotron jumbotron fluid">
+        <div className="jumbotron jumbotron-fluid">
             <div className="container">
-                <h1 className="text-center">
+                <h1 className="display-2 text-center">
                     Employee Tracker
                 </h1>
             </div>
--- src/components/Table.js
+++ src/components/Table.js
@@ -3,16 +3,16 @@ import React from "react";
 function Table(props) {
     return (
         <div className="container">
-            <table className="table table-bordered table-dark shadow">
-                <thead>
+            <table className="table table-striped">
+                <thead className="thead-dark">
                     <tr>
                         <th scope="col">Photo</th>
-                        <th scope="col"><button className="badge badge-primary" name="firstName" onClick={props.sortColumn}>First Name</button></th>
-                        <th scope="col"><button className="badge badge-primary" name="lastName" onClick={props.sortColumn}>Last Name</button></th>
-                        <th scope="col"><button className="badge badge-primary" name="email" onClick={props.sortColumn}>E-mail</button></th>
-                        <th scope="col"><button className="badge badge-primary" name="city" onClick={props.sortColumn}>City</button></th>
-                        <th scope="col"><button className="badge badge-primary" name="state" onClick={props.sortColumn}>State</button></th>
-                        <th scope="col"><button className="badge badge-primary" name="country" onClick={props.sortColumn}>Country</button></th>
+                        <th scope="col" className="text-center"><button className="badge badge-pill badge-info" name="firstName" onClick={props.sortColumn}>First Name</button></th>
+                        <th scope="col" className="text-center"><button className="badge badge-pill badge-info" name="lastName" onClick={props.sortColumn}>Last Name</button></th>
+                        <th scope="col" className="text-center"><button className="badge badge-pill badge-info" name="email" onClick={props.sortColumn}>E-mail</button></th>
+                        <th scope="col" className="text-center"><button className="badge badge-pill badge-info" name="city" onClick={props.sortColumn}>City</button></th>
+                        <th scope="col" className="text-center"><button className="badge badge-pill badge-info" name="state" onClick={props.sortColumn}>State</button></th>
+                        <th scope="col" className="text-center"><button className="badge badge-pill badge-info" name="country" onClick={props.sortColumn}>Country</button></th>
 
                     </tr>
                 </thead>
@@ -23,12 +23,12 @@ function Table(props) {
                     {props.results.map(result => (
                         <tr key={result.login.uuid}>
                             <td><img alt="employee" src={result.picture.thumbnail}></img></td>
-                            <td>{result.name.first}</td>
-                            <td>{result.name.last}</td>
-                            <td>{result.email}</td>
-                            <td>{result.location.city}</td>
-                            <td>{result.location.state}</td>
-                            <td>{result.location.country}</td>
+                            <td className="text-center">{result.name.first}</td>
+                            <td className="text-center">{result.name.last}</td>
+                            <td className="text-center">{result.email}</td>
+                            <td className="text-center">{result.location.city}</td>
+                            <td className="text-center">{result.location.state}</td>
+                            <td className="text-center">{result.location.country}</td>
                         </tr>
                     ))}
 
